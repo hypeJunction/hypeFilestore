141e1b6b899b844a655123441824c34d91f2f432
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-04-14 23:58:54 +0200

    Update icon server

--- servers/icon.php
+++ servers/icon.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once dirname(dirname(__FILE__)) . '/lib/autoloader.php';
-
 $base_dir = dirname(dirname(dirname(dirname(__FILE__))));
 
 require_once $base_dir . '/engine/settings.php';
-require_once $base_dir . '/vendor/autoload.php';
+require_once $base_dir . '/engine/lib/autoloader.php';
+
+require_once dirname(dirname(__FILE__)) . '/lib/autoloader.php';
 
 global $CONFIG;
 $conf = new \Elgg\Database\Config($CONFIG);
